84901987908658fe1c5c1c34db3174470f8f2995
Jayesh Tambe <tambe_jayesh@hotmail.com>
2017-01-18 09:27:11 -0800

    Testing Arrow functions in javscript using node.js

---
+++ ArrowFunctionsES6.js
@@ -0,0 +1,25 @@
+
+// Stupid ES5 with _this
+
+var logUpperCase = function() {
+  var _this = this
+
+  this.string = this.string.toUpperCase()
+  return function () {
+    return console.log(_this.string)
+  }
+}
+
+logUpperCase.call({ string: 'es5 sucks' })()
+
+
+//VS
+
+//ES6 does not need _this
+
+var logUpperCase = function() {
+  this.string = this.string.toUpperCase()
+  return () => console.log(this.string)
+}
+
+logUpperCase.call({ string: 'es6 is better' })()
